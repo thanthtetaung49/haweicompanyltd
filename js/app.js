25264e040bf64eca759734642e9327d8c826b97b
Thant Htet Aung <thanthtetaung12@gmail.com>
2022-07-12 21:51:34 +0630

    9:51pm commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+    // Start Header Section 
+
+    // Start navbar 
+
+    $(".navbarbuttons").click("click", function() {
+
+        $(".navbarbuttons").toggleClass("crossxs");
+    })
+    // End navbar 
+
+    // End Header Section 
+});
